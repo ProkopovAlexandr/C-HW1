f7068704c60d2d1cf49ecc6de844e7cd0a07966b
Alexandr <prok0107@mail.ru>
2022-12-04 15:18:11 +0300

    конечный проек

---
+++ 4/Program.cs
@@ -0,0 +1,25 @@
+﻿List<int> arr = new List<int>(4);
+Console.WriteLine("Введите первое число:");
+int a = Convert.ToInt32(Console.ReadLine());
+arr.Add(a);
+Console.WriteLine("Введите второе число:");
+int b = Convert.ToInt32(Console.ReadLine());
+arr.Add(b);
+Console.WriteLine("Введите третье число:");
+int c = Convert.ToInt32(Console.ReadLine());
+arr.Add(c);
+int i = 0;
+int max = 0;
+while (i <= 2)
+{
+    if (arr[i] > max)
+    {
+        max = arr[i];
+        i++;
+    }
+    else
+    {
+        i++;
+    }
+}
+Console.WriteLine("Наибольшее число:" + max);
---
+++ 8/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число:");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = 2;
+
+while (b <= a)
+{
+    Console.Write(b + " ");
+    b = b + 2;
+}
